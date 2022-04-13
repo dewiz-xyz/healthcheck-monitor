80534456c38cf7791eaf0c1d06028dc4fd15851d
Nazar Duchak <nazar@iovlabs.org>
2022-04-13 16:57:55 +0200

    chore: debug 2

--- src/cron-jobs.ts
+++ src/cron-jobs.ts
@@ -22,7 +22,7 @@ const JOBS = [
     'params': ['https://diis123-group-ui-goerli.vercel.app'],
     'interval': 10000,
     'message': {
-      'callbackUrl': process.env.DISCORD_WEBHOOK,
+      'callbackUrl': process.env.DISCORD_WEBHOOK || "https://discord.com/api/webhooks/963758659173752842/mtCjOGzIYzmXVH1CLx9snh1FxrNOCXMxEjPZL128FJ-8kAW8SzHsStBvzvgPFp4WT6yG",
       'embeds': [
         {
           'title': 'Something went wrong!',
